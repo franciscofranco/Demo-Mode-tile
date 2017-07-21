9591218e4efec1cafab6102030d69d4b275fbbe1
Willie Chalmers III <willieechalmers@gmail.com>
2017-07-21 17:03:34 -0500

    Update layout to remove redundant Toolbar
    - The Android framework already provides a Toolbar (ActionBar) for apps using the default theme
    - We don't need AppCompat for this activity because we don't need to support anything more than API 24
    - This also removes a redundant null check for getIntent()

--- app/src/main/java/com/franco/demomode/activities/MainActivity.java
+++ app/src/main/java/com/franco/demomode/activities/MainActivity.java
@@ -1,14 +1,13 @@
 package com.franco.demomode.activities;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toolbar;
 
 import com.franco.demomode.R;
 import com.franco.demomode.Utils;
@@ -19,10 +18,8 @@ import butterknife.ButterKnife;
 import static com.franco.demomode.Utils.isDumpPermissionGranted;
 import static com.franco.demomode.Utils.isWriteSecureSettingsPermissionGranted;
 
-public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.toolbar)
-    protected Toolbar toolbar;
-    @BindView(R.id.root_layout)
+public class MainActivity extends Activity {
+    @BindView(R.id.prefs_frame)
     protected View rootLayout;
 
     @Override
@@ -31,23 +28,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        setActionBar(toolbar);
-
         getFragmentManager().beginTransaction().replace(R.id.prefs_frame, new SettingsFragment()).commit();
 
-        if (getIntent() != null) {
-            if (getIntent().getAction() != null) {
-                if (getIntent().getAction().equals(Utils.MISSING_PERMISSION)) {
-                    final Snackbar snackbar = Snackbar.make(rootLayout,
-                            R.string.permissions_need_to_be_granted, Snackbar.LENGTH_INDEFINITE);
-                    snackbar.setAction(R.string.ok, new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            snackbar.dismiss();
-                        }
-                    });
-                    snackbar.show();
-                }
+        if (getIntent().getAction() != null) {
+            if (getIntent().getAction().equals(Utils.MISSING_PERMISSION)) {
+                final Snackbar snackbar = Snackbar.make(rootLayout,
+                        R.string.permissions_need_to_be_granted, Snackbar.LENGTH_INDEFINITE);
+                snackbar.setAction(android.R.string.ok, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        snackbar.dismiss();
+                    }
+                });
+                snackbar.show();
             }
         }
     }
