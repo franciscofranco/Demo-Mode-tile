79fa1e18b51ee62a55267bb573c359163d222a79
Francisco Franco <franciscofranco.1990@gmail.com>
2020-09-23 15:48:23 +0100

    Convert to Kotlin + v1.6 release

---
+++ app/src/main/java/com/franco/demomode/fragments/SettingsFragment.kt
@@ -0,0 +1,71 @@
+package com.franco.demomode.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.viewModels
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import com.franco.demomode.R
+import kotlin.time.ExperimentalTime
+
+@ExperimentalTime
+class SettingsFragment : PreferenceFragmentCompat(), Preference.OnPreferenceClickListener {
+    private lateinit var dumpPermissionPref: Preference
+    private lateinit var writeSecureSettingsPref: Preference
+
+    private val viewModel by viewModels<SettingsViewModel>()
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        addPreferencesFromResource(R.xml.settings)
+
+        dumpPermissionPref = findPreference(KEY_DUMP)!!
+        writeSecureSettingsPref = findPreference(KET_WRITE_SECURE_SETTINGS)!!
+
+        dumpPermissionPref.onPreferenceClickListener = this
+        writeSecureSettingsPref.onPreferenceClickListener = this
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.isDumpPermissionUpdates(requireContext()).observe(viewLifecycleOwner, ::renderDump)
+        viewModel.isWriteSecureSettingsPermissionUpdates(requireContext())
+                .observe(viewLifecycleOwner, ::renderWriteSecureSettings)
+    }
+
+    private fun renderDump(isGranted: Boolean) {
+        dumpPermissionPref.summary = when (isGranted) {
+            true -> getString(R.string.granted)
+            false -> getString(R.string.not_granted)
+        }
+    }
+
+    private fun renderWriteSecureSettings(isGranted: Boolean) {
+        writeSecureSettingsPref.summary = when (isGranted) {
+            true -> getString(R.string.granted)
+            false -> getString(R.string.not_granted)
+        }
+    }
+
+    override fun onPreferenceClick(preference: Preference): Boolean {
+        AlertDialog.Builder(requireActivity())
+                .setTitle(R.string.permission_request_title)
+                .setMessage(when (preference.key) {
+                    KEY_DUMP -> R.string.dump_permission_msg
+                    KET_WRITE_SECURE_SETTINGS -> R.string.write_secure_settings_permission_msg
+                    else -> throw IllegalArgumentException("well, this shouldn't ever happen")
+                })
+                .show()
+        return false
+    }
+
+    companion object {
+        private const val KEY_DUMP = "dump_permission"
+        private const val KET_WRITE_SECURE_SETTINGS = "write_secure_settings"
+    }
+}
---
+++ app/src/main/java/com/franco/demomode/tiles/DemoModeTile.kt
@@ -0,0 +1,70 @@
+package com.franco.demomode.tiles
+
+import android.content.Intent
+import android.graphics.drawable.Icon
+import android.service.quicksettings.Tile
+import android.service.quicksettings.TileService
+import com.franco.demomode.R
+import com.franco.demomode.Utils
+import com.franco.demomode.activities.MainActivity
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class DemoModeTile : TileService() {
+    override fun onStartListening() {
+        super.onStartListening()
+
+        GlobalScope.launch {
+            val isDemoModeAllowed = Utils().isDemoModeAllowed(applicationContext)
+            val isDemoMode = Utils().isDemoModeOn(applicationContext)
+
+            qsTile?.state = when {
+                !isDemoModeAllowed -> Tile.STATE_UNAVAILABLE
+                isDemoMode -> Tile.STATE_ACTIVE
+                else -> Tile.STATE_INACTIVE
+            }
+
+            qsTile?.icon = when {
+                isDemoMode -> Icon.createWithResource(applicationContext, R.drawable.ic_on)
+                else -> Icon.createWithResource(applicationContext, R.drawable.ic_off)
+            }
+
+            qsTile?.updateTile()
+        }
+    }
+
+    override fun onClick() {
+        super.onClick()
+
+        GlobalScope.launch {
+            val hasDumpPermission = Utils().isDumpPermissionGranted(applicationContext)
+            val hasWriteSecurePermission = Utils().isWriteSecureSettingsPermissionGranted(applicationContext)
+
+            when {
+                !hasDumpPermission || !hasWriteSecurePermission -> {
+                    Intent(applicationContext, MainActivity::class.java).apply {
+                        action = Utils.MISSING_PERMISSION
+                        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        startActivity(this)
+                    }
+                }
+
+                else -> {
+                    qsTile?.apply {
+                        if (state == Tile.STATE_ACTIVE) {
+                            state = Tile.STATE_INACTIVE
+                            icon = Icon.createWithResource(applicationContext, R.drawable.ic_off)
+                            Utils().disableDemoMode(applicationContext)
+                        } else {
+                            state = Tile.STATE_ACTIVE
+                            icon = Icon.createWithResource(applicationContext, R.drawable.ic_on)
+                            Utils().enableDemoMode(applicationContext)
+                        }
+
+                        updateTile()
+                    }
+                }
+            }
+        }
+    }
+}
